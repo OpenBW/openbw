f8b65e496774efa40d6ba3c671e271d9224b60ab
tscmoo <the.dengus@gmail.com>
2017-05-27 12:00:13 +0200

    Remove the -mwindows flag from FindSDL2.cmake.
    Since it makes executables GUI application (preventing console attach)

--- cmake/FindSDL2.cmake
+++ cmake/FindSDL2.cmake
@@ -124,12 +124,6 @@ IF(NOT APPLE)
 	FIND_PACKAGE(Threads)
 ENDIF(NOT APPLE)
 
-# MinGW needs an additional link flag, -mwindows
-# It's total link flags should look like -lmingw32 -lSDL2main -lSDL2 -mwindows
-IF(MINGW)
-	SET(MINGW32_LIBRARY mingw32 "-mwindows" CACHE STRING "mwindows for MinGW")
-ENDIF(MINGW)
-
 IF(SDL2_LIBRARY_TEMP)
 	# For SDL2main
 	IF(NOT SDL2_BUILDING_LIBRARY)
